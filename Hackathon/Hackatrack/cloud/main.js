7969ae4be7db28dd58286e05eb56a4c8ba6c5a32
master <master@Lukes-Mac-mini.local>
2015-07-20 19:33:59 -0700

    finally workingish backend for hackathons

--- Hackathon/Hackatrack/cloud/main.js
+++ Hackathon/Hackatrack/cloud/main.js
@@ -20,8 +20,12 @@ Parse.Cloud.job("hopeThisWorks", function(request, status) {
    // h.set("name","asd");
    // h.destroy();
 
-   // var hackathon = new Parse.Object("Hackathon");
-   // hackathon.destroy();
+   var query = new Parse.Query("Hackathon");
+   var deletionPromise = query.find().then(function(results) {
+      _.each(results, function(result) {
+         result.destroy();
+      })
+   })
 
    var promises = _.map(cities, function (city, index) {
 
@@ -59,6 +63,8 @@ Parse.Cloud.job("hopeThisWorks", function(request, status) {
       return promise;
    }); // [promise, promise]
 
+   promises.unshift(deletionPromise);
+
    Parse.Promise.when(promises).then(function (p1, p2) {
       status.success("all is good " + promises.length + " p1: " + p1 + " p2: " + p2 );
    });
@@ -75,7 +81,7 @@ function getHTTPResponseForCity(city) {
          "venue.city" : city,
          token : token,
          expand : "ticket_classes",
-         expand : "logo"
+         // expand : "logo"
       }
    });
 }
@@ -85,18 +91,35 @@ function hackathonForEvent(theEvent) {
    Parse.Cloud.useMasterKey();
    var hackathon = new Parse.Object("Hackathon");
 
-   hackathon.set("uri",  theEvent["resource_uri"]);
-   hackathon.set("url",  theEvent["url"]);
-   hackathon.set("uniqueID",  theEvent["id"]);
-   hackathon.set("name",  theEvent["name"]["text"]);
-   // hackathon.set("description",  theEvent["description"]["text"] || "None provided.");
-   // hackathon.set("status",  theEvent["status"]);
-   // hackathon.set("capacity",  String(theEvent["capacity"]));
-   // hackathon.set("logo",  (theEvent["logo"] != undefined || theEvent["logo"] != null) ? theEvent["logo"]["url"] : "http://www.ecolabelindex.com/files/ecolabel-logos-sized/no-logo-provided.png");
-   // hackathon.set("start",  moment(theEvent["start"]["utc"]));
-   // hackathon.set("end",   moment(theEvent["end"]["utc"]));
-   // hackathon.set("online",  theEvent["online_theEvent"]);
-   // hackathon.set("currency",  theEvent["currency"]);
+   hackathon.set("uri",             theEvent["resource_uri"]);
+   hackathon.set("url",             theEvent["url"]);
+   hackathon.set("uniqueID",        theEvent["id"]);
+   hackathon.set("name",            theEvent["name"]["text"]);
+   hackathon.set("description",    !theEvent["description"]["text"] ? "None provided." : theEvent["description"]["text"]);
+   hackathon.set("status",          theEvent["status"]);
+   hackathon.set("capacity", String(theEvent["capacity"]));
+   hackathon.set("logo",           (theEvent["logo"] != undefined || theEvent["logo"] != null) ? theEvent["logo"]["url"] : "http://www.ecolabelindex.com/files/ecolabel-logos-sized/no-logo-provided.png");
+   hackathon.set("start",  new Date(theEvent["start"]["utc"]));
+   hackathon.set("end",    new Date(theEvent["end"]["utc"]));
+   hackathon.set("online",          theEvent["online_theEvent"]);
+   hackathon.set("currency",        theEvent["currency"]);
+
+   var tickets = _.map(theEvent["ticket_classes"], function (item, index) {
+      return {
+         name:           item["name"],
+         cost:          (item["cost"]        ? item["cost"]["display"] : "0.00"),
+         fee:           (item["fee"]         ? item["fee"]["display"]  : "0.00"),
+         tax:           (item["tax"]         ? item["tax"]["display"]  : "0.00"),
+         description:   (item["description"] ? item["description"]     : "No description"),
+         onSaleStatus:   item["on_sale_status"],
+         donations:      item["donation"],
+         free:           item["free"]
+      };
+   });
+   // console.log(theEvent["ticket_classes"]);
+
+   hackathon.set("ticketClasses", tickets);
+
    // hackathon.set("ticketClassesNames",  assignTicketClassProperties(theEvent, ["name"]));
    // hackathon.set("ticketClassesCosts",  assignTicketClassProperties(theEvent, ["cost"]["display"]));
    // hackathon.set("ticketClassesFees",  assignTicketClassProperties(theEvent, ["fee"]["display"]));
@@ -114,6 +137,7 @@ function hackathonForEvent(theEvent) {
 function assignTicketClassProperties(hackathon, propertyName) {
    var ticketClassesArray = [];
    var tickeClasses = hackathon["ticket_classes"];
+
    if (tickeClasses != undefined) {
       _.each(tickeClasses, function(ticketClass){
 
